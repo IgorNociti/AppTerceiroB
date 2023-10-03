308faafa281db11378380eeadb238304685c81d0
aluno <aluno@L03MICRO05>
2023-10-03 09:46:05 -0300

    Add project files.

---
+++ AppTerceiroB/AppTerceiroB/Conexao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTerceiroB
+{
+    public static class Conexao
+    {
+        public static string strConexao = "server=bancomodulo.ddns.net;port=3306;user=igor.nocite;password=30625;database=bd_plano_certo";
+    }
+}
